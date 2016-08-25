bd41e91d9bdc607f1f739b7565498eb665f73df5
Olezt <lfants666@gmail.com>
2016-08-25 18:34:25 +0300

    Initial commit

---
+++ geoJSONcreator.js
@@ -0,0 +1,169 @@
+'use strict';
+
+//var PokemonGO = require('./poke.io.js');
+var PokemonGO = require('pokemon-go-node-api');
+var replace = require("replace");
+var fs = require('fs');
+// using var so you can login with multiple users
+var a = new PokemonGO.Pokeio();
+var pokemons=[];
+//Set environment variables or replace placeholder text
+var username = process.env.PGO_USERNAME || 'USER';
+var password = process.env.PGO_PASSWORD || 'PASSWD';
+var provider = process.env.PGO_PROVIDER || 'ptc';
+var direction = "up";
+var sw_coords = {
+            lat: 37.984998, 
+            lon: 23.733532 
+        };
+var ne_coords = {
+            lat: sw_coords.lat+0.01, 
+            lon: sw_coords.lon+0.01
+        };
+var location = {
+    type: 'coords',
+    //name: process.env.PGO_LOCATION || 'imittou 229 athens greece'
+    coords: {
+        latitude: sw_coords.lat, 
+        longitude: sw_coords.lon,
+        altitude: 0
+    }
+};
+var firstpokemon = true;
+var walkbeat = 0.0005;
+
+
+
+
+a.init(username, password, location, provider, function (err) {
+    if (err)
+        throw err;
+        fs.writeFile("pokemons_rawdata.json", '{ "type": "FeatureCollection","features": [');
+    console.log('[i] Current location: ' + a.playerInfo.locationName);
+    console.log('[i] lat/long/alt: : ' + a.playerInfo.latitude + ' ' + a.playerInfo.longitude + ' ' + a.playerInfo.altitude);
+
+
+
+    a.GetProfile(function (err, profile) {
+        
+        if (err)
+            throw err;
+
+        console.log('[i] Username: ' + profile.username);
+        setInterval(function(){
+
+
+            a.Heartbeat(function (err, hb) {
+                if (err) {
+                    console.log(err);
+                }
+                next_move();
+if(hb!=null) {
+search_for_pokemons(hb);
+}
+
+            });
+        //};//
+        }, 4000);
+        //fs.appendFile("pokemons_rawdata.json", ']}');
+
+    });
+    
+    
+    
+});
+
+function search_for_pokemons(hb) {
+    
+    var i,
+    current_pokemon_proto,
+    current_pokemon_data={},
+    seen=false;
+
+                for (i = hb.cells.length - 1; i >= 0; i--) {
+                    
+                    if (hb.cells[i].WildPokemon[0]) {
+                        seen=false;
+                        
+                        for (var j = 0; j < pokemons.length; j++) {
+                            if (hb.cells[i].WildPokemon[0].EncounterId === pokemons[j].encounter_id) {
+                                seen=true;
+                                continue;
+                            }
+                        }
+                        
+                        if (seen === false) {
+                            current_pokemon_proto = PokemonGO.pokemonlist[parseInt(hb.cells[i].WildPokemon[0].pokemon.PokemonId) - 1];
+                            var will_dissapear = new Date(Date.now() + hb.cells[i].WildPokemon[0].TimeTillHiddenMs);
+                            current_pokemon_data={disappear_time:will_dissapear.getTime(),encounter_id:hb.cells[i].WildPokemon[0].EncounterId,
+                                latitude:hb.cells[i].WildPokemon[0].Latitude,longitude:hb.cells[i].WildPokemon[0].Longitude,
+                                pokemon_name:current_pokemon_proto.name,pokemon_img:current_pokemon_proto.img,spawnpoint_id:hb.cells[i].WildPokemon[0].SpawnPointId};
+                            pokemons.push(current_pokemon_data);
+                            firstpokemon=printJSON(firstpokemon,current_pokemon_data);
+                        }
+                    }
+                }
+    
+}
+
+
+
+function printJSON(firstpokemon,pokemon) {
+        console.log('Wild '+ pokemon.pokemon_name + ' appeared');
+        var jsonPokemon='{ "type": "Feature","geometry": {"type": "Point", "coordinates": ['+pokemon.longitude+','+pokemon.latitude+']},"properties": '+JSON.stringify(pokemon)+'}]}';
+        
+        if (firstpokemon !== true) {
+            replace({
+                regex: "(]})$",
+                replacement: ","+jsonPokemon,
+                paths: ['./pokemons_rawdata.json'],
+                recursive: false,
+                silent: true
+            });
+        }else{
+            fs.appendFile("pokemons_rawdata.json", jsonPokemon);
+        }
+        return false;
+    }; 
+
+function next_move() {
+    switch (direction) {
+                    case "up":
+                        //console.log("up "+a.playerInfo.latitude );
+                        a.playerInfo.latitude += walkbeat;
+                        a.playerInfo.latitude = (a.playerInfo.latitude).toFixed(6);
+                        a.playerInfo.latitude = parseFloat(a.playerInfo.latitude);
+                        if (a.playerInfo.latitude >= ne_coords.lat) {
+                            direction = "down";
+                            if (a.playerInfo.longitude <= ne_coords.lon) {
+                                a.playerInfo.longitude += walkbeat;
+                                a.playerInfo.longitude = (a.playerInfo.longitude).toFixed(6);
+                                a.playerInfo.longitude = parseFloat(a.playerInfo.longitude);
+                            } else {
+                                a.playerInfo.longitude -= walkbeat;
+                                a.playerInfo.longitude = (a.playerInfo.longitude).toFixed(6);
+                                a.playerInfo.longitude = parseFloat(a.playerInfo.longitude);
+                            }
+                        }
+                        break;
+                    case "down":
+                        //console.log("down "+a.playerInfo.latitude );
+                        a.playerInfo.latitude -= walkbeat;
+                        a.playerInfo.latitude = (a.playerInfo.latitude).toFixed(6);
+                        a.playerInfo.latitude = parseFloat(a.playerInfo.latitude);
+                        if (a.playerInfo.latitude <= sw_coords.lat) {
+                            direction = "up";
+                            if (a.playerInfo.longitude <= ne_coords.lon) {
+                                a.playerInfo.longitude += walkbeat;
+                                a.playerInfo.longitude = (a.playerInfo.longitude).toFixed(6);
+                                a.playerInfo.longitude = parseFloat(a.playerInfo.longitude);
+                            } else {
+                                a.playerInfo.longitude -= walkbeat;
+                                a.playerInfo.longitude = (a.playerInfo.longitude).toFixed(6);
+                                a.playerInfo.longitude = parseFloat(a.playerInfo.longitude);
+                            }
+                        }
+                        break;
+                }
+
+}

a27aa138cc44a859d5cd107954779a2c14d0d74f
Olezt <lfants666@gmail.com>
2016-08-28 00:23:33 +0300

    Add var distanceto

--- config.js
+++ config.js
@@ -1,18 +1,22 @@
+var fs = require('fs');
 //Set environment variables or replace placeholder text
-username = process.env.PGO_USERNAME || 'USERNAME';
-password = process.env.PGO_PASSWORD || 'PASSWORD';
+username = process.env.PGO_USERNAME || 'USER';
+password = process.env.PGO_PASSWORD || 'PASSWD';
 
 //Southwest corner of area to scan
 sw_coords = {
             lat: 37.984998,  //EXAMPLE
             lon: 23.733532   //EXAMPLE
         };
+
+distanceTo=0.02;
 //Northeast corner of area to scan
 ne_coords = {
-            lat: sw_coords.lat+0.01, 
-            lon: sw_coords.lon+0.01
+            lat: sw_coords.lat+distanceTo, 
+            lon: sw_coords.lon+distanceTo
 };
 
 //Create javascript file that is imported in index.html
 fs.writeFile("coords.js","var sw_coordlat="+ sw_coords.lat+";\n");
-fs.appendFile("coords.js","var sw_coordlon="+ sw_coords.lon+";");
+fs.appendFile("coords.js","var sw_coordlon="+ sw_coords.lon+";\n");
+fs.appendFile("coords.js","var distanceTo="+ distanceTo+";");

375bb1feb555a0422da34e5f82e75bb6a93708e3
olezt <lfants666@gmail.com>
2016-08-28 01:47:58 +0300

    enable cli multiword location

--- config.js
+++ config.js
@@ -5,15 +5,19 @@ username = process.env.PGO_USERNAME || 'USER';
 password = process.env.PGO_PASSWORD || 'PASSWD';
 distanceTo=0.01;
 
-if(process.argv[2]!==null){
-      name= process.env.PGO_LOCATION || process.argv[2];
-}
-
 //Southwest corner of area to scan
 sw_coords = {
             lat: 37.984998,  //EXAMPLE
             lon: 23.733532   //EXAMPLE
         };
+        
+if(process.argv[2]!==null){
+      var location="";
+      for(var i=2;i<process.argv.length;i++){
+            location=location+" "+process.argv[i];
+      }
+            name= process.env.PGO_LOCATION || location;
+}
 
 //Northeast corner of area to scan
 ne_coords = {

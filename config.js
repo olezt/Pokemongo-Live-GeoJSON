7954d44dfae072da9095d8fd7e4784907af481f2
Olezt <lfants666@gmail.com>
2016-08-28 01:28:30 +0300

    add cli name var

--- config.js
+++ config.js
@@ -1,7 +1,13 @@
 var fs = require('fs');
+
 //Set environment variables or replace placeholder text
 username = process.env.PGO_USERNAME || 'USER';
 password = process.env.PGO_PASSWORD || 'PASSWD';
+distanceTo=0.01;
+
+if(process.argv[2]!==null){
+      name= process.env.PGO_LOCATION || process.argv[2];
+}
 
 //Southwest corner of area to scan
 sw_coords = {
@@ -9,14 +15,8 @@ sw_coords = {
             lon: 23.733532   //EXAMPLE
         };
 
-distanceTo=0.02;
 //Northeast corner of area to scan
 ne_coords = {
             lat: sw_coords.lat+distanceTo, 
             lon: sw_coords.lon+distanceTo
 };
-
-//Create javascript file that is imported in index.html
-fs.writeFile("coords.js","var sw_coordlat="+ sw_coords.lat+";\n");
-fs.appendFile("coords.js","var sw_coordlon="+ sw_coords.lon+";\n");
-fs.appendFile("coords.js","var distanceTo="+ distanceTo+";");
